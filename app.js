58d3049067db6da9073e930a386287b07cf36e03
ahsansamad <ingeniouslogo@gmail.com>
2024-03-27 23:34:35 +0500

    table and table  len

--- app.js
+++ app.js
@@ -5,6 +5,12 @@
 // var arr = ["0123","1012","2101"];
 // document.write(`${arr[0]} <br> ${arr[1]} <br> ${arr[2]}`)
 
-for(i = 1; i <= 10;i++){
-    document.write(`${i} <br>`)
-}
+// for(i = 1; i <= 10;i++){
+//     document.write(`${i} <br>`)
+// }
+
+// var tableOf = +prompt("enter which table you want to run")
+// var tableLen = +prompt("enter the table length you want to run")
+// for(i = 1 ;i <= tableLen; i++){
+//     document.write(`${tableOf} x ${i} = ${tableOf * i}  <br>`)
+// }
